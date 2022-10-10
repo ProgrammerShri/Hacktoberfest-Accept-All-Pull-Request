5eb8c50d1cc8aed2036166b4dbaa1cf124d9a455
vijay dhyani <vijay.dhyani@ballisticlearning.com>
2022-10-10 11:13:10 +0530

    notification-added

---
+++ notification-page/script.js
@@ -0,0 +1,6 @@
+function readmsg() {
+    let test = document.getElementById("read");
+    test.setAttribute("class", "noti d-flex align-items-center read-bg");
+    // alert('working')
+    // console.log(test);
+}
